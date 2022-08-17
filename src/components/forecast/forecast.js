9c9b6ee2a8014d43636a11d3ba23fce344ffd73d
Chuksco <70770519+Chuksco@users.noreply.github.com>
2022-08-17 14:33:43 +0100

    update

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,22 +1,24 @@
-import { 
-    Accordion, 
+import {
+    Accordion,
     AccordionItemHeading,
-    AccordionItem, 
-    AccordionItemPanel, 
-    AccordionItemButton 
+    AccordionItem,
+    AccordionItemPanel,
+    AccordionItemButton
 } from "react-accessible-accordion";
 
 
 const Forecast = ({ data }) => {
     return (
         <>
-            <label className="title">Daitly</label>
+            <label className="title">Daily</label>
             <Accordion allowZeroExpanded>
-                {data.list.splice(0, 7).map((item, idx) => (
+                {data?.list?.splice(0, 7).map((item, idx) => (
                     <AccordionItem key={idx}>
                         <AccordionItemHeading>
                             <AccordionItemButton>
-                                    Hello
+                                <div className="daily-item">
+                                    <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
+                                </div>
                             </AccordionItemButton>
                         </AccordionItemHeading>
                         <AccordionItemPanel></AccordionItemPanel>
