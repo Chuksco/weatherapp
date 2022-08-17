0211a2ab5b4d403d7b6c1e24c1acaf8704e4750c
Chuksco <70770519+Chuksco@users.noreply.github.com>
2022-08-17 09:14:35 +0100

    first commit

---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,31 @@
+import { 
+    Accordion, 
+    AccordionItemHeading,
+    AccordionItem, 
+    AccordionItemPanel, 
+    AccordionItemButton 
+} from "react-accessible-accordion";
+
+
+const Forecast = ({ data }) => {
+    return (
+        <>
+            <label className="title">Daitly</label>
+            <Accordion allowZeroExpanded>
+                {data.list.splice(0, 7).map((item, idx) => (
+                    <AccordionItem key={idx}>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                    Hello
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel></AccordionItemPanel>
+                    </AccordionItem>
+
+                ))}
+            </Accordion>
+        </>
+    )
+}
+
+export default Forecast;
