ad88d382b812d536fe9bcfb730b16ac0c9d02311
Chuksco <70770519+Chuksco@users.noreply.github.com>
2022-08-23 07:55:05 +0100

    update

---
+++ src/components/footer/footer.js
@@ -0,0 +1,9 @@
+import "./footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer"> Designed with love by <a href="#"> Chukwukamso Onu</a></div>
+    )
+}
+
+export default Footer;
