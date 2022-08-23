1c1c59fe3485834f9fbdea24205cfe74b41ccdd6
Chuksco <70770519+Chuksco@users.noreply.github.com>
2022-08-23 07:56:10 +0100

    update

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,9 @@ import "./footer.css";
 
 const Footer = () => {
     return (
-        <div className="footer"> Designed with love by <a href="#"> Chukwukamso Onu</a></div>
+        <div className="footer"> Designed with love by
+            <a href="#"> Chukwukamso Onu</a>
+        </div>
     )
 }
 
